c56f59b92b2963d96a84c9e1c2313420caabbcee
lucasmanea21 <lucasmanea@Yahoo.com>
2021-04-18 17:08:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Router>
+    <Navbar toggle={toggle} isOpen={isOpen}/>
+    <Sidebar toggle={toggle} isOpen={isOpen}/>
+    </Router>
+    </>
   );
 }
 
